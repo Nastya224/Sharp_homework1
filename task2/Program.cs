a4ef6b51468326c6858a02ad0644799842116a78
Nastya_keks <aagolunova@edu.hse.ru>
2022-09-25 22:19:29 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("число 1:");
+string a = Console.ReadLine();
+int number1 = int.Parse(a);
+
+Console.Write("число 2:");
+string b = Console.ReadLine();
+int number2 = int.Parse(b);
+
+Console.Write("число 3:");
+string c = Console.ReadLine();
+int number3 = int.Parse(c);
+
+int max = number1;
+if (number2 >= max) max = number2;
+if (number3 >= max) max = number3;
+Console.Write("max= ");
+    Console.WriteLine(max);
