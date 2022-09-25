b16bca90afbd27e1a6661c62defe4d218a29730b
Nastya_keks <aagolunova@edu.hse.ru>
2022-09-25 22:44:39 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("число :");
+string a = Console.ReadLine();
+double number1 = double.Parse(a);
+
+if (number1 % 2==0)
+{
+Console.Write(number1);
+Console.WriteLine("-> да");
+           
+}
+else
+{
+Console.Write(number1);
+Console.WriteLine("-> нет");
+}
