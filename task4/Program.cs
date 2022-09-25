c78b69367efeb90d5fb5225235e4290e332e1b4a
Nastya_keks <aagolunova@edu.hse.ru>
2022-09-25 23:16:28 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число:");
+int N = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+while (count <= N)
+{
+    if (count % 2==0)
+    {Console.Write($"{count}"); 
+    }
+count = count+1;
+}
